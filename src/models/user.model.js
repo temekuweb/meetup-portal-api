02899f449854248f7aa0f49f0f6e2a7f6709b2b8
Byron Coughlin <byroncoughlinsr@gmail.com>
2018-10-26 09:46:58 -0700

    more code cleanup per travis recommendations

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,27 +2,30 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function(app) {
+module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
-  var validateEmail = function(email) {
+  var validateEmail = function (email) {
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(email);
   };
-  
+
   const user = new Schema(
     {
-    email:    { unique: [true, 'Email already exists'],    
-      type: String,  
-      Required:  [true, 'Email address cannot be left blank.'],
-      validate: [ validateEmail, 'Please fill a valid email address'],
-                  match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+      email: {
+        unique: [true, 'Email already exists'],
+        type: String,
+        Required: [true, 'Email address cannot be left blank.'],
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
       },
-  
-    password: { type: String , 
-                required: [true,  'Password cannot be left blank']}
+
+      password: {
+        type: String,
+        required: [true, 'Password cannot be left blank']
+      }
     },
-    
+
     {
       timestamps: true
     }
